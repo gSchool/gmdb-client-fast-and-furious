bc7af5857888bbd628aa29a5ffffef85e8615c35
mohancore256 <mohan.core256@gmail.com>
2019-04-30 15:39:51 -0400

    adding test cases

--- movie/src/app/app.module.ts
+++ movie/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
