949827754960b891cde8030744b3b847aabbebe2
Hinal5775 <hinal.shah2@cognizant.com>
2019-04-30 16:10:18 -0400

    added test description

--- movie/src/app/movie-list/movie-list.component.spec.ts
+++ movie/src/app/movie-list/movie-list.component.spec.ts
@@ -22,4 +22,39 @@ describe('MovieListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  xit('single movie should exist', ()=>{
+
+  });
+  
+  xit('text input should exist and take input', ()=>{
+
+  });
+  xit('search button should exist', ()=>{
+
+  });
+  xit('search button should send valid api request', ()=>{
+
+  });
+  xit('search title should return movie(s)', ()=>{
+
+  });
+  xit('search actor should return movie(s)', ()=>{
+
+  });
+  xit('search keyword should return movie(s)', ()=>{
+
+  });
+  xit('should show 0 results if api response has 0 results', ()=>{
+
+  });
+  xit('should show 1 result if api response has 1 result', ()=>{
+
+  });
+  xit('if response from api has more than 10 results should show 10 results and show more button', ()=>{
+
+  });
+  xit('if show more is clicked then we show all of the results', ()=>{
+
+  });
 });
