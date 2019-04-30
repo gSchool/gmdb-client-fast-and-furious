812680c0fd884fccfdb4786cc9e451798fac2e08
mohancore256 <mohan.core256@gmail.com>
2019-04-30 16:20:19 -0400

    Second commit test cases

--- movie/src/app/movie-detail/movie-detail.component.spec.ts
+++ movie/src/app/movie-detail/movie-detail.component.spec.ts
@@ -23,8 +23,27 @@ describe('MovieDetailComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should have an image', () => {
+    const e1 = fixture.nativeElement;
+    expect(e1.querySelector('div.poster').innerText).toContain('poster1');
+  });
 
+  it('should have a title', () => {
+    const e1 = fixture.nativeElement;
+    
+    expect(e1.querySelector('div.title').innerText).toBeTruthy('Coco');
+  });
+
+  xit('should have a movie-info', () => {
+    const e1 = fixture.nativeElement;
+    expect(e1.querySelector('div.').innerText).toBeTruthy();
+  });
+
+  xit('should have a plot', () => {
+    
     expect(component).toBeTruthy();
   });
+
+
 
 });
