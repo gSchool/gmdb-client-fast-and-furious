bf37b44df4f4cbdb266442bd77d0c0eb2509cab1
ajnijv81 <ajnijv81@gmail.com>
2019-05-01 16:44:19 -0400

    Add routing

--- movie/src/app/app-routing.module.ts
+++ movie/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movies/search', component: MovieListComponent },
+  { path: 'movies/detail/:name', component: MovieDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
