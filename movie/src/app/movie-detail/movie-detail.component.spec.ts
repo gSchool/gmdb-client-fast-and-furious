db1ad66de6fd52eef35e4b4d258f4c71f7d5ac72
mohancore256 <mohan.core256@gmail.com>
2019-05-01 15:23:32 -0400

    added test cases with passing part-2

--- movie/src/app/movie-detail/movie-detail.component.spec.ts
+++ movie/src/app/movie-detail/movie-detail.component.spec.ts
@@ -3,6 +3,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MovieDetailComponent } from './movie-detail.component';
 import { Movie } from '../movie';
 
+function escapeHtml(unsafe) {
+  return unsafe
+       .replace(/&/g, "&amp;")
+       .replace(/</g, "&lt;")
+       .replace(/>/g, "&gt;")
+       .replace(/"/g, "&quot;")
+       .replace(/'/g, "&#039;");
+};
+
 describe('MovieDetailComponent', () => {
   let component: MovieDetailComponent;
   let fixture: ComponentFixture<MovieDetailComponent>;
@@ -25,18 +34,6 @@ describe('MovieDetailComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('text input should exist', ()=>{
-  //   //setup
-  //   //exercise
-  //     //expect
-  //     let expected = 1; //1 or more
-  //     //actual
-  //     let actual = fixture.elementRef.nativeElement.querySelectorAll('input[name=q]').length;
-  //   //assert
-  //   expect(actual).toBeGreaterThanOrEqual(expected);
-  //   expect(fixture.elementRef.nativeElement.querySelector('input[name=q]')).toBeTruthy();
-  //   //teardown
-  // });
 
   it('should have an image', () => {
   //setup
@@ -139,17 +136,139 @@ describe('MovieDetailComponent', () => {
      //expect
      let expected1 = "Internet Movie Database"; 
      let expected2 = "7.0/10"; 
+     let expectedSources = 3;
+     let expectedValues = 3;
      //actual
      let actual1 = fixture.elementRef.nativeElement.querySelectorAll('.ratings > .rating > .source')[0].innerHTML;
      let actual2 = fixture.elementRef.nativeElement.querySelectorAll('.ratings > .rating > .value')[0].innerHTML;
+     let actualSources = fixture.elementRef.nativeElement.querySelectorAll('.ratings > .rating > .source').length;
+     let actualValues = fixture.elementRef.nativeElement.querySelectorAll('.ratings > .rating > .value').length;
      //assert
      expect(actual1).toEqual(expected1);
      expect(actual2).toEqual(expected2);
+     expect(actualSources).toEqual(expectedSources);
+     expect(actualValues).toEqual(expectedValues)
    });
 
-  xit('should have a plot', () => {
-    const e1 = fixture.nativeElement;
-    expect(e1.querySelector('div.textarea').innerText).toBeTruthy('');
+   it('should have a director', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = "Kenneth Branagh"; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.director')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+
+   it('should have a writer', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = "Ashley Miller (screenplay), Zack Stentz (screenplay), Don Payne (screenplay), J. Michael Straczynski (story), Mark Protosevich (story), Stan Lee (comic book), Larry Lieber (comic book), Jack Kirby (comic book)"; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.writer')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+
+   it('should have a actors', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = "Chris Hemsworth, Natalie Portman, Tom Hiddleston, Anthony Hopkins"; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.actors')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+   
+
+   it('should have a released', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = "06 May 2011"; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.released')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+
+   it('should have a runtime', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = "115 min"; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.runtime')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+
+   it('should have a language', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = "English"; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.language')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+
+   it('should have a country', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = "USA"; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.country')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+
+   it('should have a awards', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = escapeHtml("5 wins & 30 nominations."); 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.awards')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+
+   it('should have a type', () => {
+    
+    //setup
+     //exercise
+     //expect
+     let expected = "movie"; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.type')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
+   });
+
+
+  it('should have a plot', () => {
+     //setup
+     //exercise
+     //expect
+     let expected = "The powerful, but arrogant god Thor, is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders."; 
+     //actual
+     let actual = fixture.elementRef.nativeElement.querySelectorAll('.plot')[0].innerHTML;
+     //assert
+     expect(actual).toEqual(expected);
   });
 
 
