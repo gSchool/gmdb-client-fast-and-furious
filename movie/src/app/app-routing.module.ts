7cd6dba31e3ea5b2ac6743fbd1702f70fc4acc55
ajnijv81 <ajnijv81@gmail.com>
2019-05-02 10:49:44 -0400

    Add homepage redirect

--- movie/src/app/app-routing.module.ts
+++ movie/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: "/movies/search", pathMatch:"full"},
   { path: 'movies/search', component: MovieListComponent },
   { path: 'movies/detail/:name', component: MovieDetailComponent }
 ];
