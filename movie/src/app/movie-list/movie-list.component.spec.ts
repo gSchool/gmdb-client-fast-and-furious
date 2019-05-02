d22b8521d59bb7d7d9ea17e4113f100a914936fe
mohancore256 <mohan.core256@gmail.com>
2019-05-02 12:54:35 -0400

    Fix movie list tests

--- movie/src/app/movie-list/movie-list.component.spec.ts
+++ movie/src/app/movie-list/movie-list.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MovieListComponent } from './movie-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SingleMovieComponent } from '../single-movie/single-movie.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('MovieListComponent', () => {
   let component: MovieListComponent;
@@ -11,6 +12,7 @@ describe('MovieListComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        RouterTestingModule,
         ReactiveFormsModule
       ],
       declarations: [ 
